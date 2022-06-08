c4803abad7b502c728ead161f68ee39b0d24439f
Sabiha Bary <sabihaab9@gmail.com>
2022-06-08 15:08:24 -0400

    commit tree cb12e3be11c56db05053788fd1dd800484bd778c

--- src/components/BlogContent.js
+++ src/components/BlogContent.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 function BlogContent(props) {
+
   return <div id="blog-content">{props.articleText}</div>;
 }
 
--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -5,6 +5,15 @@ import Comment from "./Comment";
 function BlogPost() {
   return (
     <div id="blog-post">
+    
+      //The syntax for creating props for a React component
+      // is the same as the syntax for writing attributes for an HTML tag.
+
+
+      //we render the BlogContent component and...
+      //create a prop called articleText in parent component ("BlogPost") that we assign a value of "Dear Reader..." 
+      //this value is accessible from within the BlogContent component as props.articleText
+
       <BlogContent articleText="Dear Reader: Bjarne Stroustrup has the perfect lecture oration." />
       <Comment commentText="I agree with this statement. - Angela Merkel" />
       <Comment commentText="A universal truth. - Noam Chomsky" />
